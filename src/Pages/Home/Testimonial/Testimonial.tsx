0fcaf53e6c53739b2f28a704018b5076b59ff87e
Pobarul-islam <pobarul3@gmail.com>
2022-07-21 18:52:20 +0600

    4 slider img added

--- src/Pages/Home/Testimonial/Testimonial.tsx
+++ src/Pages/Home/Testimonial/Testimonial.tsx
@@ -42,6 +42,66 @@ const Testimonial = () => {
                         <div className='text2'><blockquote>-- Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt veniam et voluptatum fugiat aperiam corporis explicabo nostrum ducimus ex.</blockquote></div>
                   </div>
                 </div></SwiperSlide>
+                <SwiperSlide><div className="slider">
+                    <div className="firstSlider">
+                        <img className='imgSize' src="https://i.ibb.co/R9sFBNW/tamara-bellis-e-DVQw-VMLMg-U-unsplash-removebg-preview.png" alt="" />
+                        
+                    </div>
+                   
+                   
+                    <div>
+                        <h4><hr />
+                            <img src={logsvg} alt="" />
+                        </h4>
+                    <div className='text'>
+                       
+                       <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br /> Culpa natus sit odit eum? Fugiat sed quaerat eius itaque <br /> saepe nesciunt officiis, labore voluptates iusto veritatis in <br /> vero temporibus rem ad officia facere accusamus eos <br /> ducimus, ipsam tempore blanditiis expedita. Labore.</p>
+                            
+                        </div>
+                        <div className='text2'><blockquote>-- Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt veniam et voluptatum fugiat aperiam corporis explicabo nostrum ducimus ex.</blockquote></div>
+                  </div>
+                </div></SwiperSlide>
+                <SwiperSlide><div className="slider">
+                    <div className="firstSlider">
+                        <img className='imgSize' src="https://i.ibb.co/R9sFBNW/tamara-bellis-e-DVQw-VMLMg-U-unsplash-removebg-preview.png" alt="" />
+                        
+                    </div>
+                   
+                   
+                    <div>
+                        <h4><hr />
+                            <img src={logsvg} alt="" />
+                        </h4>
+                    <div className='text'>
+                       
+                       <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br /> Culpa natus sit odit eum? Fugiat sed quaerat eius itaque <br /> saepe nesciunt officiis, labore voluptates iusto veritatis in <br /> vero temporibus rem ad officia facere accusamus eos <br /> ducimus, ipsam tempore blanditiis expedita. Labore.</p>
+                            
+                        </div>
+                        <div className='text2'><blockquote>-- Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt veniam et voluptatum fugiat aperiam corporis explicabo nostrum ducimus ex.</blockquote></div>
+                  </div>
+                </div></SwiperSlide>
+                <SwiperSlide><div className="slider">
+                    <div className="firstSlider">
+                        <img className='imgSize' src="https://i.ibb.co/R9sFBNW/tamara-bellis-e-DVQw-VMLMg-U-unsplash-removebg-preview.png" alt="" />
+                        
+                    </div>
+                   
+                   
+                    <div>
+                        <h4><hr />
+                            <img src={logsvg} alt="" />
+                        </h4>
+                    <div className='text'>
+                       
+                       <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br /> Culpa natus sit odit eum? Fugiat sed quaerat eius itaque <br /> saepe nesciunt officiis, labore voluptates iusto veritatis in <br /> vero temporibus rem ad officia facere accusamus eos <br /> ducimus, ipsam tempore blanditiis expedita. Labore.</p>
+                            
+                        </div>
+                        <div className='text2'><blockquote>-- Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt veniam et voluptatum fugiat aperiam corporis explicabo nostrum ducimus ex.</blockquote></div>
+                  </div>
+                </div></SwiperSlide>
     
       ...
     </Swiper>
