b0a4fc60edbd222c0ea1c844656281ee72922538
mirajul alam <alifmirajulalam@gmail.comm>
2022-07-22 00:40:18 +0600

    bugsearch section added

--- src/Pages/Home/Home/Home.tsx
+++ src/Pages/Home/Home/Home.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Bugsearch from '../Bugsearch/Bugsearch';
 import Teamwork from '../Teamwork/Teamwork';
 
 const Home = () => {
     return (
         <div>
-            
+            <Bugsearch/>
             <Teamwork/>
         </div>
     );
