20810acf71fb7a1fd2a400389f47f4f0527afc45
najuml-bd <najmul0612@gmail.com>
2022-07-26 19:13:50 +0600

    banner section added

--- src/Pages/Home/Home/Home.tsx
+++ src/Pages/Home/Home/Home.tsx
@@ -5,10 +5,12 @@ import Bugsearch from '../Bugsearch/Bugsearch';
 import Teamwork from '../Teamwork/Teamwork';
 import ProcessManagement from '../ProcessManagement/ProcessManagement';
 import Testimonial from '../Testimonial/Testimonial';
+import Banner from '../Banner/Banner';
 
 const Home = () => {
     return (
         <div>
+            <Banner />
             <Bugsearch />
             <Teamwork />
             {/* <IndexSlider /> */}
