6c5e69df684ca33441b15f7ea16294ebd82879ba
John Doe <hamedhasan422421@gmail.com>
2022-07-26 19:08:02 +0300

    something update

--- src/Pages/Home/components/VideoSection.tsx
+++ src/Pages/Home/components/VideoSection.tsx
@@ -47,7 +47,7 @@ const VideoSection = () => {
         </div>
             <p className='my-3'>App Rate</p>
          <CountUp
-         end={400}
+         end={800}
          duration={5}
          />
           </div>
