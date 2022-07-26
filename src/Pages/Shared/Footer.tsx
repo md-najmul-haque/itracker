afef2de6785c8b70e22036e8cc40b4145a68e2c5
John Doe <hamedhasan422421@gmail.com>
2022-07-26 19:53:39 +0300

    update on footer

--- src/Pages/Shared/Footer.tsx
+++ src/Pages/Shared/Footer.tsx
@@ -26,8 +26,8 @@ const Footer = () => {
 </div>
 
 
-<svg className='relative none lg:block z-0' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#000106" fill-opacity="1" d="M0,224L48,197.3C96,171,192,117,288,90.7C384,64,480,64,576,85.3C672,107,768,149,864,181.3C960,213,1056,235,1152,213.3C1248,192,1344,128,1392,96L1440,64L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg> 
 
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#000106" fill-opacity="1" d="M0,128L80,133.3C160,139,320,149,480,165.3C640,181,800,203,960,218.7C1120,235,1280,245,1360,250.7L1440,256L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
 
 <footer className="text-gray-600 body-font md:pt-36 lg:pt-0">
   <div className="container px-5 pb-11 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
