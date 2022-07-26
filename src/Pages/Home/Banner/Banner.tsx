9fd60998c7b4b208c7c4d845672355a9ffb2f338
najuml-bd <najmul0612@gmail.com>
2022-07-26 22:00:08 +0600

    banner text updated

--- src/Pages/Home/Banner/Banner.tsx
+++ src/Pages/Home/Banner/Banner.tsx
@@ -9,7 +9,7 @@ const Banner = () => {
 
         <section>
             <div className="w-full relative pb-10 px-6 xl:px-0">
-                <img className="absolute bg-left-top w-full inset-0 h-full" src="https://cdn.tuk.dev/assets/templates/weCare/hero2-bg.png" alt="we care family" />
+                <img className="absolute w-full inset-0 h-full" src="https://i.ibb.co/GckN6y6/hero2-bg.png" alt="we care family" />
                 <nav className="lg:hidden relative z-40">
                     <div className="flex py-6 justify-between items-center px-4">
                         <div>
@@ -75,33 +75,15 @@ const Banner = () => {
                         </div>
                     </div>
                 </nav>
-                <div className="pt-32 lg:flex  items-center relative z-10 container mx-auto">
+                <div className="pt-32 lg:flex lg:flex-row-reverse items-center relative z-10 container mx-auto">
                     <div className="w-full lg:w-1/2 h-full lg:pr-10 xl:pr-0">
                         <img aria-label="people smiling" className="mx-auto" src="https://cdn.tuk.dev/assets/templates/weCare/hero2-left.png" alt="people smiling" />
                     </div>
                     <div role="contentinfo" className="w-full lg:w-1/2 h-full">
-                        <p className="text-indigo-700 uppercase text-2xl mb-4">Qusique Tincidun sapien</p>
-                        <h1 className="text-indigo-700 text-4xl lg:text-6xl font-black mb-8">All natural ingredients</h1>
-                        <p className="text-gray-800 font-regular mb-8">COMBINING SCIENCE WITH BEAUTY. L'Oreal Paris offerincare products is developed and rigorously tested with leading scientists. Indulge yourself in a luxurious and sensorial skincare experience. Cutting-edge innovations, proven by Science.</p>
-                        <div className="bg-white lg:mt-16 py-4 px-4 flex flex-col sm:flex-row justify-start sm:justify-between items-start sm:items-center shadow-lg rounded-lg">
-                            <div className="sm:flex items-center py-2">
-                                <div className="flex items-center">
-                                    <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/right_aligned_with_searchbar_Svg4.svg" alt="icon" />
-                                    <input aria-label="Doctor name" className="w-24 xl:w-32 leading-none tracking-normal text-gray-800 ml-2.5 placeholder-black" placeholder="Doctor Name" />
-                                </div>
-                                <div className="flex items-center sm:mx-4 xl:mx-14 my-6 lg:my-0">
-                                    <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/right_aligned_with_searchbar_Svg5.svg" alt="icon" />
-                                    <input aria-label="zip code" className="w-24 xl:w-32 leading-none tracking-normal text-gray-800 ml-2.5 placeholder-black" placeholder="Zip code" />
-                                </div>
-                                <div className="flex items-center">
-                                    <img src="https://tuk-cdn.s3.amazonaws.com/can-uploader/right_aligned_with_searchbar_Svg6.svg" alt="icon" />
-                                    <input aria-label="insurance" className="w-24 xl:w-32 leading-none tracking-normal text-gray-800 ml-2.5 placeholder-black" placeholder="Insurance" />
-                                </div>
-                            </div>
-                            <button aria-label="search" className="focus:bg-indigo-700 focus:ring-indigo-700 focus:ring-2 focus:ring-offset-2 text-white bg-indigo-600 hover:bg-indigo-700 mt-4 sm:mt-0 p-3 lg:-ml-8 rounded w-full sm:w-auto relative">
-                                <img className="absolute right-0 mr-2 sm:mr-auto sm:relative icon icon-tabler icon-tabler-search cursor-pointer" src="https://tuk-cdn.s3.amazonaws.com/can-uploader/right_aligned_with_searchbar_Svg7.svg" alt="search" />
-                                <input aria-label="search" className="sm:hidden border-b border-gray-300 w-full bg-transparent pr-6" />
-                            </button>
+                        <p className="uppercase text-4xl lg:text-6xl mb-4">The easiest way to track your issue</p>
+                        <p className="mb-8 text-3xl">You’ve arrived at the central place for work—add team members, drop your work in Asana, start collaborating. Yep, it’s that easy.</p>
+                        <div className="flex justify-center">
+                            <button className="btn btn-outline btn-primary">Get Started</button>
                         </div>
                     </div>
                 </div>
