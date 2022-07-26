8bb6303bd9db69b501d604518cf7865e8a9d3d8f
mirajul alam <alifmirajulalam@gmail.comm>
2022-07-26 10:15:27 +0600

    bugsearching components update

--- src/Pages/Home/Teamwork/Teamwork.tsx
+++ src/Pages/Home/Teamwork/Teamwork.tsx
@@ -12,7 +12,7 @@ const Teamwork = () => {
         </Typography>
         <Typography align="center" component="p" variant="h6" sx={{pt:3}}>
           With Backlog, you can keep bug and issue tracking under one roof.
-          Developers can <br /> easily collaborate on and release code, tracking each step via pull-requests right in <br /> issues. Git and Subversion
+          Developer can <br /> easily collaborate on and release code, tracking each step via pull-requests right in <br /> issues. Git and Subversion
           repositories keep teams connected through it all.
         </Typography>
       </Box>
