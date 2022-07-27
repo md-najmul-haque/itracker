edfd38306bba0e8bc5b2cc03873f2a87dad26c4e
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-07-27 20:54:27 +0600

    create new branch

---
+++ src/Pages/Home/ProcessMangements/ProcessMangements.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProcessMangements = () => {
+    return (
+        <div>
+            <h1>ProcessMangements</h1>
+        </div>
+    );
+};
+
+export default ProcessMangements;
