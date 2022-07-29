72974069c4a09da8273ad56cb7159a027ed2a19b
mirajul alam <alifmirajulalam@gmail.comm>
2022-07-29 23:07:30 +0600

    loading added

--- src/Pages/Home/Home/Home.tsx
+++ src/Pages/Home/Home/Home.tsx
@@ -5,10 +5,13 @@ import Teamwork from '../Teamwork/Teamwork';
 import ProcessManagement from '../ProcessManagement/ProcessManagement';
 import Testimonial from '../Testimonial/Testimonial';
 import Banner from '../Banner/Banner';
+import Loading from '../../Shared/Loading/Loading';
 
 const Home = () => {
+    
     return (
-        <div>       
+        <div>
+            <Loading/>       
             <Banner />
             <Bugsearch/>
             <Teamwork />
