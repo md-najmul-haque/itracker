91c101ceb151f96f33caf7b4008f4d2f3e665eb1
Md Najmul Haque <najmulhaque008@gmail.com>
2022-07-30 18:45:50 +0600

    banner updated

--- src/Pages/Home/Banner/Banner.tsx
+++ src/Pages/Home/Banner/Banner.tsx
@@ -6,12 +6,11 @@ const Banner = () => {
     const [menu, setMenu] = useState<boolean>(false);
 
     return (
-        <section>
+        <section className="relative bg-white z-2">
             <div className="w-full h-screen relative pb-10 px-6 xl:px-0">
-                <img className="absolute w-full inset-0 h-full" src="https://i.ibb.co/GckN6y6/hero2-bg.png" alt="we care family" />
                 <div className="pt-32 lg:flex lg:flex-row-reverse items-center justify-center relative z-10 container mx-auto">
                     <div className="w-full lg:w-1/2 h-full lg:pr-10 xl:pr-0">
-                        <img aria-label="people smiling" className="mx-auto" src="https://cdn.tuk.dev/assets/templates/weCare/hero2-left.png" alt="people smiling" />
+                        <img aria-label="people smiling" className="mx-auto" src="https://img.freepik.com/free-vector/business-team-discussing-ideas-startup_74855-4380.jpg?w=1380&t=st=1659181029~exp=1659181629~hmac=84b5dacb1d295dfaba82d35c37c66f1d7d7bdd771db3ffa58c31dc86fa3296f9" alt="people smiling" />
                     </div>
                     <div role="contentinfo" className="w-full lg:w-1/2 h-full">
                         <p className="uppercase text-4xl lg:text-6xl mb-4">The easiest way to track your issue</p>
@@ -20,6 +19,9 @@ const Banner = () => {
                             <Link to='/signup' className="btn btn-outline btn-primary">Get Started</Link>
                         </div>
                     </div>
+
+                </div>
+                <div className="h-screen bg-slate-100 w-6/12 absolute top-0 right-0 z-[-1]">
 
                 </div>
             </div>
