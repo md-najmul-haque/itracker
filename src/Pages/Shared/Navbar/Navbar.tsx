054ad3ca725ce0fc3a9f7d5031478987b25c4d9f
Md Najmul Haque <najmulhaque008@gmail.com>
2022-07-31 21:20:23 +0600

    navber updated

--- src/Pages/Shared/Navbar/Navbar.tsx
+++ src/Pages/Shared/Navbar/Navbar.tsx
@@ -31,7 +31,7 @@ const Navbar = () => {
     return (
 
         <div className="navbar bg-primary mx-auto sticky top-0 z-50">
-            <div className='container mx-auto'>git
+            <div className='container mx-auto'>
                 <div className="navbar-start">
                     <div className="dropdown">
                         <label className="btn btn-ghost lg:hidden">
@@ -49,7 +49,7 @@ const Navbar = () => {
                     </ul>
                 </div>
                 <div className="navbar-end  md:hidden flex">
-                <label htmlFor="dashboard-sidebar" className="btn btn-primary drawer-button lg:hidden"><svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg></label>
+                    <label htmlFor="dashboard-sidebar" className="btn btn-primary drawer-button lg:hidden"><svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg></label>
                 </div>
             </div>
         </div>
