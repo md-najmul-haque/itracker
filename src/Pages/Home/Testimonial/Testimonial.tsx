713dc43c0b7c531e2a431efba6492993511a42ca
Pobarul-islam <pobarul3@gmail.com>
2022-08-01 13:19:17 +0600

    some code removed

--- src/Pages/Home/Testimonial/Testimonial.tsx
+++ src/Pages/Home/Testimonial/Testimonial.tsx
@@ -163,11 +163,7 @@ const Testimonial = () => {
               <h4 className="font-bold text-2xl">Lara Pia</h4>
                   <p>Web developer</p>
                   <div className="rating rating-md">
-  <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
-  <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" checked />
-  <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
-  <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
-  <input type="radio" name="rating-7" className="mask mask-star-2 bg-orange-400" />
+
 </div>
               <div>
                
