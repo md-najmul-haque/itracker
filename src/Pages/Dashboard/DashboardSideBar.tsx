ff9edc9c4337428dc5df2ea70dad1594a54797da
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-08-02 14:21:47 +0600

    change dashobard color

--- src/Pages/Dashboard/DashboardSideBar.tsx
+++ src/Pages/Dashboard/DashboardSideBar.tsx
@@ -8,7 +8,7 @@ import {AiFillHome } from 'react-icons/ai';
 type MyComponentProps = React.PropsWithChildren<{}>;
 const DashboardSideBar = ({ children, ...other}: MyComponentProps) => {
     return (
-        <div className="drawer drawer-mobile bg-base-200">
+        <div className="drawer drawer-mobile bg-white">
             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content ">
                 {/*  <!-- Page content here --> */}
