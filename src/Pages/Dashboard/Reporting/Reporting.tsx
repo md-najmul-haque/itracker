e80c2c5263610822957b6854ca476d745fdbb38e
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-08-03 12:27:08 +0600

    add apex chart for show task

--- src/Pages/Dashboard/Reporting/Reporting.tsx
+++ src/Pages/Dashboard/Reporting/Reporting.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReportCard from './ReportCard';
+import ReportChart from './ReportChart';
 
 const Reporting = () => {
     return (
         <div className='m-5'>
             <ReportCard />
+            <ReportChart/>
         </div>
     );
 };
