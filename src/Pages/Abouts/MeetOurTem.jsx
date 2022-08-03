64ca730514f2398f93a55b48d4ad2991eefb30ea
Pobarul-islam <pobarul3@gmail.com>
2022-08-03 16:11:39 +0600

    json file maped

--- src/Pages/Abouts/MeetOurTem.jsx
+++ src/Pages/Abouts/MeetOurTem.jsx
@@ -6,23 +6,7 @@ const MeetOurTem = ({ team }) => {
         <div className='text-center'>
 <div className="card w-96 bg-white shadow-xl">
     
-                <div className="card-body">
-                <div class="avatar avatarimg">
-  <div class="w-24 memberImg rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-    <img src="https://placeimg.com/192/192/people" />
-  </div>
-</div>
-    <h2 className="card-title name"> {name} </h2>
-    <p> {Position} </p>
-    <div className='icon'>
-    <i className="fa-brands fa-facebook"></i>
-    <i className="fa-brands ml-3 fa-github"></i>
-    <i className="fa-solid ml-3 fa-envelope"></i>
-    <i class="fa-brands ml-3 fa-linkedin"></i>
-   </div>
-    
-    
-  </div>
+                
 </div>
 
         </div>
