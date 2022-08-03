09d3d70fb27086b18151120a5a3120f8b885c162
John Doe <hamedhasan422421@gmail.com>
2022-08-03 18:58:05 +0300

    update on footer

--- src/Pages/Shared/Footer.tsx
+++ src/Pages/Shared/Footer.tsx
@@ -3,8 +3,8 @@ import '../../main.css'
 const Footer = () => {
   return (
     <>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#1E293B" fill-opacity="1" d="M0,224L120,229.3C240,235,480,245,720,224C960,203,1200,149,1320,122.7L1440,96L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"></path></svg>
-      <footer className="footer pb-10 bg-slate-800 text-base-content">
+      {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#1E293B" fill-opacity="1" d="M0,224L120,229.3C240,235,480,245,720,224C960,203,1200,149,1320,122.7L1440,96L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"></path></svg> */}
+      <footer className="footer pt-20 pb-9 bg-secondary text-base-content">
         <div className='pl-0 lg:pl-40'>
           <span className="footer-title">Services</span>
           <a className="link link-hover">Branding</a>
@@ -29,11 +29,11 @@ const Footer = () => {
           <span className="footer-title">Newsletter</span>
           <div className="form-control w-80">
             <label className="label">
-              <span className="label-text">Enter your email address</span>
+              <span className="label-text text-white">Enter your email address</span>
             </label>
             <div className="relative">
-              <input type="text" placeholder="username@site.com" className="input input-bordered w-full pr-16" />
-              <button className="btn bg-[#00ADB5] absolute top-0 right-0 rounded-l-none">Subscribe</button>
+              <input type="text" placeholder="username@site.com" className="input  w-full pr-16" />
+              <button className="btn bg-neutral absolute top-0 right-0 rounded-l-none">Subscribe</button>
             </div>
           </div>
         </div>
