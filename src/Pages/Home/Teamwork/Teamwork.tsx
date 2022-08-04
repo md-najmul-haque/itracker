25d00554fd9d97834960593d47588650e31ec061
mirajul alam <alifmirajulalam@gmail.comm>
2022-08-04 11:25:47 +0600

    bug search update

--- src/Pages/Home/Teamwork/Teamwork.tsx
+++ src/Pages/Home/Teamwork/Teamwork.tsx
@@ -1,31 +1,22 @@
 import React from 'react';
-import { Box, Container, Typography } from "@mui/material";
-import CardMedia from '@mui/material/CardMedia';
 import teamwork from '../../../Assets/teamwork.gif';
 
 const Teamwork = () => {
 
   return (
-  <Container fixed sx={{pb:5}}>
-      <Box sx={{ my: 5 }}>
-        <Typography align="center" variant="h5" sx={{ fontWeight: "bold" }}>
-          Work on team project work
-        </Typography>
-        <Typography align="center" component="p" variant="h6" sx={{ pt: 3 }}>
-          With Backlog, you can keep bug and issue tracking under one roof.
-          Developer can <br /> easily collaborate on and release code, tracking each step via pull-requests right in <br /> issues. Git and Subversion
-          repositories keep teams connected through it all.
-        </Typography>
-      </Box>
-      <Box sx={{ width: "75%", mx: "auto" }}>
-        <CardMedia
-          sx={{ my: "5" }}
-          component="img"
-          src={teamwork}
-        >
-        </CardMedia>
-      </Box>
-    </Container>
+  <>
+    <div className="hero  bg-neutral text-white">
+  <div className="hero-content text-center">
+    <div className='lg:w-4/6'>
+      <h1 className="text-3xl font-bold py-3">Work on team project work</h1>
+      <p className="py-6 font-semibold"> With Backlog, you can keep bug and issue tracking under one roof.
+          Developer can  easily collaborate on and release code, tracking each step via pull-requests right in issues. Git and Subversion
+          repositories keep teams connected through it all.</p>
+      <img src={teamwork} alt="" className="w-full" />
+    </div>
+  </div>
+</div>
+    </>
   );
 };
 
