1d15417037fd9f68f251f3a8a0e143b1d1314aef
Md Najmul Haque <najmulhaque008@gmail.com>
2022-08-05 10:47:08 +0600

    user loging data sent to the server form signup page

--- src/Pages/Authentication/SignUp/SignUp.tsx
+++ src/Pages/Authentication/SignUp/SignUp.tsx
@@ -34,9 +34,8 @@ function SignUp() {
         createUserWithEmailAndPassword(email, password);
 
         const createUser = {
-            name: data.name,
-            email: data.email,
-            password: data.password
+            userName: data.name,
+            email: email
         }
 
         fetch('http://localhost:5000/signup',
@@ -65,8 +64,28 @@ function SignUp() {
         return <Loading />
     }
 
+    if (gUser || gitUser) {
+
+        const createUser = {
+            userName: gUser?.user.displayName || gitUser?.user.displayName,
+            email: gUser?.user.email || gitUser?.user.email,
+        }
+
+        fetch('http://localhost:5000/signup',
+            {
+                method: "POST",
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(createUser)
+            })
+            .then(res => res.json())
+
+    }
+
     if (user || gUser || gitUser) {
         console.log(user || gUser || gitUser)
+        console.log(gUser?.user.displayName)
         navigate('/')
     }
 
