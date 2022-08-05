f2c6d655bf6d5cfa7a512d78ff9d10c1dba9bd18
Pobarul-islam <pobarul3@gmail.com>
2022-08-05 20:52:56 +0600

    3 card added

--- src/Pages/About/Counter/Counter.tsx
+++ src/Pages/About/Counter/Counter.tsx
@@ -4,7 +4,32 @@ const Counter = () => {
     return (
         <div>
             <h1 className='text-center text-4xl '>Our Coustomer</h1>
-            
+             <div className='grid grid-cols-3 ml-14 mb-14 mt-20'>
+        
+        <div className="card w-96 bg-base-100 shadow-xl  bg-orange-400 text-white hover:bg-sky-700 hover:text-white  hover:bg-sky-700 hover:text-white ease-in duration-300">
+            <div className="card-body">
+                <h1 className='text-center text-3xl'>Total User</h1>
+<h2 className="card-title counter ml-24"></h2>
+
+<div className="card-actions justify-end">
+ 
+</div>
+</div>
+</div>
+        <div className="card w-96 bg-base-100 shadow-xl  bg-orange-400 text-white hover:bg-sky-700 hover:text-white  hover:bg-sky-700 hover:text-white ease-in duration-300">
+            <div className="card-body">
+                <h1 className='text-center text-3xl'>Continue User</h1>
+<h2 className="card-title counter ml-24"></h2>
+
+<div className="card-actions justify-end">
+ 
+</div>
+</div>
+</div>
+   
+    
+
+    </div>
        </div>
        
     );
