9f3df7c8349681e5af34aac8011fc16daed02076
Pobarul-islam <pobarul3@gmail.com>
2022-08-05 14:07:29 +0600

    some code clean

--- src/Pages/Abouts/Abouts.tsx
+++ src/Pages/Abouts/Abouts.tsx
@@ -15,9 +15,11 @@ const Abouts = () => {
   },[])
     return (
         <div>
-    <div className="hero min-h-screen bg-base-10">
+    <div className="hero min-h-screen bg-base-10 ">
   <div className="hero-content flex-col lg:flex-row-reverse">
-    <img src="https://i.ibb.co/MDZh73c/785104-removebg-preview.png" className="max-w-sm rounded-lg " />
+      <div>
+            <img src="https://i.ibb.co/MDZh73c/785104-removebg-preview.png" className="max-w-sm rounded-lg " />
+    </div>
     <div style={{width:"50%"}}>
       <h1 className="text-5xl font-bold">What is iTracker ?</h1>
       <p className="py-6">iTracker is a powerful, feature-rich and mature error-tracking system, or bug-tracking system. Defect-tracking systems allow teams of developers to effectively track outstanding bugs, issues, problems, enhancements, and other change requests in their products. Simple bug-tracking capabilities are often built into integrated source code management environments such as Github or other web-based or locally installed equivalents.</p>
