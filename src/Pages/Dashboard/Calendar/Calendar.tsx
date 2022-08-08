da722e8830ba72a782e493092dadfe9b28f7d317
Pobarul-islam <pobarul3@gmail.com>
2022-08-08 19:39:49 +0600

    some code added and commented

--- src/Pages/Dashboard/Calendar/Calendar.tsx
+++ src/Pages/Dashboard/Calendar/Calendar.tsx
@@ -1,12 +1,35 @@
-import React from 'react';
-import FullCalendar from '@fullcalendar/react'
+import React, { useEffect, useState } from 'react';
+import FullCalendar, { EventInput } from '@fullcalendar/react'
+import interactionPlugin, { DateClickArg } from '@fullcalendar/interaction'
 import dayGridPlugin from '@fullcalendar/daygrid';
+// import { Query, onSnapshot } from '@tanstack/react-query';
 const Calendar = () => {
+    const [data, setData] = useState<any[]>([]);
+    // useEffect(() => {
+        // const q = Query(collection(db, 'non_existent'))
+        // const unsub = onSnapshot(q, (snap) => {
+        //     const array: any[] = snap.docs.map(doc => {
+    //             return {
+    //                 id: doc.id,
+    //                 title: doc.get('title'),
+    //                 start: doc.get('start').toDate(),
+    //                 allDay: doc.get('allDay')
+    //             }
+    //         });
+    //         setData([...array]);
+    //     })
+    // },[])
+    
+    const handleDateClick = (e:DateClickArg) => {
+       if(e.jsEvent.altKey) console.log('Alt Click')
+    }
     return (
         <div className='mt-20'>
-            <h1>Hello calendar</h1>
+           
             <FullCalendar
-                  plugins={[ dayGridPlugin ]}
+                events={data}
+                plugins={[dayGridPlugin, interactionPlugin]}
+                dateClick = {handleDateClick}
             />
           
         </div>
