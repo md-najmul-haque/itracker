fe06f3656635d8867dbb8c553ece319414cb828c
Pobarul-islam <pobarul3@gmail.com>
2022-08-08 17:35:06 +0600

    calendar added

--- src/Pages/Dashboard/Calendar/Calendar.tsx
+++ src/Pages/Dashboard/Calendar/Calendar.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid';
 const Calendar = () => {
     return (
         <div className='mt-20'>
             <h1>Hello calendar</h1>
+            <FullCalendar
+                  plugins={[ dayGridPlugin ]}
+            />
+          
         </div>
     );
 };
