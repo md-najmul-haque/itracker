87579904a46c35de34f1d63f23e55665febc710b
Pobarul-islam <pobarul3@gmail.com>
2022-08-09 14:00:45 +0600

    click to open dialogbox added

--- src/Pages/Dashboard/Calendar/Calendar.tsx
+++ src/Pages/Dashboard/Calendar/Calendar.tsx
@@ -3,15 +3,44 @@ import FullCalendar, { EventInput } from '@fullcalendar/react'
 import interactionPlugin, { DateClickArg } from '@fullcalendar/interaction'
 import dayGridPlugin from '@fullcalendar/daygrid';
 import AddEvent from './AddEvent';
+
+
+
 const Calendar = () => {
+    const[show, setShow] = useState(false)
     const [data, setData] = useState<any[]>([]);
 
     
-    const handleDateClick = (dateClickInfo:any) => {
-        alert(dateClickInfo.dateStr)
+    // const handleDateClick = (dateClickInfo:any) => {
+    //     alert(dateClickInfo.dateStr)
+    // }
+
+    let clicks = 0;
+    let timer = setTimeout(() => { }, 200);
+    let dateClicked = '';
+    const reset = () => {
+        clicks = 0;
+        clearTimeout(timer);
+        dateClicked = '';
     }
+    const handleDateClick = (e: DateClickArg) => {
+        clicks++;
+        if (clicks===1) {
+            dateClicked = e.dateStr;
+            timer = setTimeout(() => {
+                alert(`You Click On Single Calendar ${e.dateStr}`)
+                reset();
+            }, 500);
+        } else {
+            if (dateClicked === e.dateStr) {
+               setShow(true)
+            }
+            reset();
+        }
+        }
+
     return <>
-        <AddEvent/>
+      {show && <AddEvent/>}
              <div className='mt-20'>
         
         <FullCalendar
