c8ad180944e00c899abad7ea1826cc79a0219dd8
Md Najmul Haque <najmulhaque008@gmail.com>
2022-08-09 15:25:25 +0600

    updated file

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -2,8 +2,6 @@ import { configureStore } from '@reduxjs/toolkit'
 import rootReducer from '../reducers/index'
 
 
-const reducer = () => { }
-
 const store = configureStore({ reducer: rootReducer });
 
 export default store; 
