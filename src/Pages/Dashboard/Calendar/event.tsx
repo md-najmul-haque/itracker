912a7c355b65cfc4418d6f2632bc310fd38edf7a
Pobarul-islam <pobarul3@gmail.com>
2022-08-11 08:56:35 +0600

    two event added

--- src/Pages/Dashboard/Calendar/event.tsx
+++ src/Pages/Dashboard/Calendar/event.tsx
@@ -4,7 +4,17 @@ let eventGuid = 0;
 let todayStr = new Date().toISOString().replace(/T.*$/, ""); // YYYY-MM-DD of today
 export const createEventId = () => String(eventGuid++);
 export const INITIAL_EVENTS: EventInput[] = [
-
+  {
+    id: createEventId(),
+    title: "All-day event",
+    start: todayStr
+  },
+  {
+    id: createEventId(),
+    title: "Timed event",
+      start: todayStr + "T12:00:00",
+   
+  }
 
 ];
 
