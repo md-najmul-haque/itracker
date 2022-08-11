cc753bb556bc95ebd045b101aa12ebb7eb3f2ede
Md Najmul Haque <najmulhaque008@gmail.com>
2022-08-11 11:59:30 +0600

    calender component updated

--- src/Pages/Dashboard/Calendar/Calendar.tsx
+++ src/Pages/Dashboard/Calendar/Calendar.tsx
@@ -1,77 +1,71 @@
-import React from 'react';
 import { useCallback, useState } from "react";
-import FullCalendar, {
-  DateSelectArg,
-  EventApi,
-  EventClickArg
-} from "@fullcalendar/react";
+import FullCalendar, { DateSelectArg, EventApi, EventClickArg } from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
-
-
 import interactionPlugin from "@fullcalendar/interaction";
 import { INITIAL_EVENTS, createEventId } from "./event";
-const Calendarr = () => {
-    const [currentEvents, setCurrentEvents] = useState<EventApi[]>([]);
-    const handleEvents = useCallback(
-      (events: EventApi[]) => setCurrentEvents(events),
-      []
-    );
-    const handleDateSelect = useCallback((selectInfo: DateSelectArg) => {
-        let title = prompt("Add Your Event")?.trim();
-        
-      let calendarApi = selectInfo.view.calendar;
-      calendarApi.unselect();
-      if (title) {
-        calendarApi.addEvent({
-          id: createEventId(),
-          title,
-          start: selectInfo.startStr,
-          end: selectInfo.endStr,
-          allDay: selectInfo.allDay
-        });
-      }
-    }, []);
-    const handleEventClick = useCallback((clickInfo: EventClickArg) => {
-      if (
-        window.confirm(`Do you want to delete it?${clickInfo.event.title}`)
-      ) {
-        clickInfo.event.remove();
-      }
-    }, []);
-    return (
-        <div className="demo-app mt-20 mx-5">
+
+const Calendar = () => {
+  const [currentEvents, setCurrentEvents] = useState<EventApi[]>([]);
+  const handleEvents = useCallback(
+    (events: EventApi[]) => setCurrentEvents(events),
+    []
+  );
+  const handleDateSelect = useCallback((selectInfo: DateSelectArg) => {
+    let title = prompt("Add Your Event")?.trim();
+
+    let calendarApi = selectInfo.view.calendar;
+    calendarApi.unselect();
+    if (title) {
+      calendarApi.addEvent({
+        id: createEventId(),
+        title,
+        start: selectInfo.startStr,
+        end: selectInfo.endStr,
+        allDay: selectInfo.allDay
+      });
+    }
+  }, []);
+  const handleEventClick = useCallback((clickInfo: EventClickArg) => {
+    if (
+      window.confirm(`Do you want to delete it?${clickInfo.event.title}`)
+    ) {
+      clickInfo.event.remove();
+    }
+  }, []);
+  return (
+    <div className="demo-app mt-20 mx-5">
       <div className="demo-app-main">
         <FullCalendar
           plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
           initialView="dayGridMonth"
           selectable={true}
           editable={true}
-                    initialEvents={INITIAL_EVENTS}
-                    firstDay={1}
-                    weekends={true}
+          initialEvents={INITIAL_EVENTS}
+          firstDay={1}
+          weekends={true}
 
-                    headerToolbar={{
-                        center: "title",
-                        left: "dayGridMonth, timeGridWeek, timeGridDay"
-                        
-                    }}
-                    buttonText={{
-                        day: "Day",
-                        prev: "Previous",
-                        next: "Next",
-                        week: "Week",
-                        month: "Month",
-                        today: "Today",
-                       
-                    }}
+          headerToolbar={{
+            center: "title",
+            left: "dayGridMonth, timeGridWeek, timeGridDay"
+
+          }}
+          buttonText={{
+            day: "Day",
+            prev: "Previous",
+            next: "Next",
+            week: "Week",
+            month: "Month",
+            today: "Today",
+
+          }}
           eventsSet={handleEvents}
           select={handleDateSelect}
           eventClick={handleEventClick}
-                />
+        />
       </div>
     </div>
-    );
+  );
 };
 
-export default Calendarr;
+export default Calendar;
