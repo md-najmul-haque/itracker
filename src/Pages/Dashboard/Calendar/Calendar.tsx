66bccea33b7fc50b783c70afdddf3c3761df5968
Pobarul-islam <pobarul3@gmail.com>
2022-08-11 08:53:13 +0600

    calendar added and displayed

--- src/Pages/Dashboard/Calendar/Calendar.tsx
+++ src/Pages/Dashboard/Calendar/Calendar.tsx
@@ -12,7 +12,41 @@ import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import { INITIAL_EVENTS, createEventId } from "./event";
 const Calendarr = () => {
-
+    const [currentEvents, setCurrentEvents] = useState<EventApi[]>([]);
+    const handleEvents = useCallback(
+      (events: EventApi[]) => setCurrentEvents(events),
+      []
+    );
+    const handleDateSelect = useCallback((selectInfo: DateSelectArg) => {
+        let title = prompt("Add Your Event")?.trim();
+        
+      let calendarApi = selectInfo.view.calendar;
+      calendarApi.unselect();
+      if (title) {
+        calendarApi.addEvent({
+          id: createEventId(),
+          title,
+          start: selectInfo.startStr,
+          end: selectInfo.endStr,
+          allDay: selectInfo.allDay
+        });
+      }
+    }, []);
+    const handleEventClick = useCallback((clickInfo: EventClickArg) => {
+      if (
+        window.confirm(`Do you want to delete it?${clickInfo.event.title}`)
+      ) {
+        clickInfo.event.remove();
+      }
+    }, []);
+    return (
+        <div className="demo-app mt-20 mx-5">
+      <div className="demo-app-main">
+        <FullCalendar
+          plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
+          initialView="dayGridMonth"
+          
+                />
       </div>
     </div>
     );
