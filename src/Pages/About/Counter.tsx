7b99d8344a4cf8ded1bcfafc490fa9fedead361d
Pobarul-islam <pobarul3@gmail.com>
2022-08-12 09:50:50 +0600

    counter background added

--- src/Pages/About/Counter.tsx
+++ src/Pages/About/Counter.tsx
@@ -6,8 +6,10 @@ const Counter = () => {
    
    
     return (
-        <div className='bg-white shadow-xl  p-5 mx-5 mb-10 pb-20'>
-            <h2 className='text-center text-4xl text-primary  font-bold'>Project Completed</h2>
+        <div>
+                <h2 className='text-center text-4xl text-primary  font-bold'>Project Completed</h2>
+            <div className='bg-white  p-5 mx-5 mb-10 pb-20 counterBackground'>
+
           
             <div className='mb-5 mx-8  grid lg:grid-cols-4 md:grid-cols-2 gap-10 mt-20'>
 
@@ -18,25 +20,26 @@ const Counter = () => {
             <p className=' text-gray-400 '>Completed Project</p> 
             </div>
                 
-            <div className='card border-solid drop-shadow-lg  hover:bg-sky-700 hover:text-white ease-in duration-300 gap-2 pb-3 w-70 h-60 align-center justify-center text-center bg-white  shadow-xl'>
+            <div className='card border-solid tobBorder drop-shadow-lg  hover:bg-sky-700 hover:text-white ease-in duration-300 gap-2 pb-3 w-70 h-60 align-center justify-center text-center bg-white  shadow-xl'>
             <p className='text-violet-300 text-6xl'><i className="fa-solid fa-heart"></i></p>
             <CountUp className='text-4xl' end={150} duration={5} enableScrollSpy={true} />
             <p className='text-gray-400 '>Happy Clients</p>   
             </div>
                 
-            <div className='card border-solid drop-shadow-lg hover:bg-sky-700 hover:text-white ease-in duration-300 gap-2 pb-3 w-70 h-60 align-center justify-center text-center bg-white   shadow-xl'>
+            <div className='card border-solid tobBorder drop-shadow-lg hover:bg-sky-700 hover:text-white ease-in duration-300 gap-2 pb-3 w-70 h-60 align-center justify-center text-center bg-white   shadow-xl'>
             <p className='text-violet-300 text-6xl'><i className="fa-solid fa-users"></i></p>
             <CountUp className='text-4xl'  end={500} duration={5} enableScrollSpy={true} />
             <p className='text-gray-400 '>Total Users</p>
             </div>
                 
-            <div className='card border-solid drop-shadow-lg hover:bg-sky-700 hover:text-white ease-in duration-300 gap-2 pb-3 w-70 h-60 align-center justify-center text-center bg-white   shadow-xl'>
+            <div className='card border-solid tobBorder drop-shadow-lg hover:bg-sky-700 hover:text-white ease-in duration-300 gap-2 pb-3 w-70 h-60 align-center justify-center text-center bg-white   shadow-xl'>
             <p className='text-violet-300 text-6xl'><i className="fa-solid fa-award"></i></p>
             <CountUp className='text-4xl' end={50} duration={5} enableScrollSpy={true} />
             <p className='text-gray-400'>Awards</p>
             </div>
                 
             </div>
+        </div>
         </div>
     );
 };
