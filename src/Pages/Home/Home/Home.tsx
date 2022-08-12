73222e04ad8b15c1c278a14a51a7f409d1614efb
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-08-12 19:22:10 +0600

    update branch

--- src/Pages/Home/Home/Home.tsx
+++ src/Pages/Home/Home/Home.tsx
@@ -18,7 +18,6 @@ const Home = () => {
             <Teamwork />
             <IssueManagement />
             <Bugsearch />
-
             <TrustedCompany />
             <Resources/>
             <FAQ />
