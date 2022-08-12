2b18afb522577d89075ce312ee6ff7df0ba42f8f
Pobarul-islam <pobarul3@gmail.com>
2022-08-12 08:39:20 +0600

    two card added

--- src/Pages/About/Counter.tsx
+++ src/Pages/About/Counter.tsx
@@ -17,6 +17,13 @@ const Counter = () => {
             <CountUp className='text-4xl' end={100} duration={5} enableScrollSpy={true} />
             <p className=' text-gray-400 '>Completed Project</p> 
             </div>
+                
+            <div className='card border-solid drop-shadow-lg  hover:bg-sky-700 hover:text-white ease-in duration-300 gap-2 pb-3 w-70 h-60 align-center justify-center text-center bg-white  shadow-xl'>
+            <p className='text-violet-300 text-6xl'><i className="fa-solid fa-heart"></i></p>
+            <CountUp className='text-4xl' end={150} duration={5} enableScrollSpy={true} />
+            <p className='text-gray-400 '>Happy Clients</p>   
+            </div>
+                
             
                 
             </div>
