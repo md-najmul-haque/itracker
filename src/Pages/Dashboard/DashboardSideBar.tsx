b3a163ffd0e77e30e25d38831712a32757049fba
Md Najmul Haque <najmulhaque008@gmail.com>
2022-08-15 10:53:20 +0600

    project name showed in side bar

--- src/Pages/Dashboard/DashboardSideBar.tsx
+++ src/Pages/Dashboard/DashboardSideBar.tsx
@@ -1,62 +1,76 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { MdOutlineTaskAlt } from 'react-icons/md';
+import { MdOutlineTaskAlt, MdOutlineVideoCall, MdVideoCall } from 'react-icons/md';
 import { AiFillHome } from 'react-icons/ai';
 import { AiFillProject } from 'react-icons/ai';
 import { RiUserStarLine } from 'react-icons/ri';
+import Loading from '../Shared/Loading/Loading';
+import { useQuery } from '@tanstack/react-query';
 
 type DashboardSideBarProps = React.PropsWithChildren<{}>;
 
-
-// type DashboardSideBarProps = {
-//     project: {
-//         projectName: string,
-//         projectDescription: string,
-//         email: string,
-//         startingDate: string,
-//         endData: string
-//     },
-//     children: React.PropsWithChildren<{}>
-// }
+type Project = {
+    projectName: string,
+}
 
 const DashboardSideBar = ({ children }: DashboardSideBarProps) => {
 
+    const { data: projects, isLoading, error, refetch } = useQuery(['projects'], () =>
+        fetch('http://localhost:5000/getProject')
+            .then(res => res.json())
+    )
+
+    if (isLoading) {
+        return <Loading />
+    }
 
     return (
-        <div className="drawer drawer-mobile bg-white">
-            <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-            <div className="drawer-content ">
-                {/*  <!-- Page content here --> */}
-                {children}
-            </div>
-            <div className="drawer-side ">
-                <label form="my-drawer-2" className="drawer-overlay"></label>
-                <ul className="menu p-4 overflow-y-auto w-60 bg-accent text-base-content">
-                    <h1 className='mb-4 text-center text-white font-bold text-2xl'>ITRACKER</h1>
-                    {/*   Sidebar content here */}
-                    <li className=' hover:bg-slate-600 transition-all rounded-lg'>
-                        <Link className='bg-transparent text-white' to='/'> <AiFillHome />HOME</Link>
-                    </li>
+        <>
+            <div className="drawer drawer-mobile bg-white">
+                <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+                <div className="drawer-content ">
+                    {/*  <!-- Page content here --> */}
+                    {children}
+                </div>
+                <div className="drawer-side bg-accent">
+                    <label form="my-drawer-2" className="drawer-overlay"></label>
+                    <ul className="menu p-4 overflow-y-auto w-60 text-base-content">
+                        <h1 className='mb-4 text-center text-white font-bold text-2xl'>ITRACKER</h1>
+                        {/*   Sidebar content here */}
+                        <li className=' hover:bg-slate-600 transition-all rounded-lg'>
+                            <Link className='bg-transparent text-white' to='/'> <AiFillHome />HOME</Link>
+                        </li>
 
-                    <li className=' hover:bg-slate-600 transition-all rounded-lg'>
-                        <Link className='bg-transparent text-white' to="/dashboard/repoting"><MdOutlineTaskAlt /> Repoting</Link>
-                    </li>
-                    <li className=' hover:bg-slate-600 transition-all rounded-lg'>
-                        <Link className='bg-transparent text-white' to="/dashboard/add-reviews"><RiUserStarLine /> Add Reviews</Link>
-                    </li>
-                    <li className=' hover:bg-slate-600 transition-all rounded-lg'>
-                        <Link className='bg-transparent text-white' to="/dashboard/calendar"><AiFillProject />Calendar</Link>
-                    </li>
-                    <li className=' hover:bg-slate-600 transition-all rounded-lg'>
-                        <Link className='bg-transparent text-white' to="/dashboard/meeting"><AiFillProject />Meeting</Link>
-                    </li>
-                    <li className=' hover:bg-slate-600 transition-all rounded-lg'>
-                        <Link className='bg-transparent text-white' to="/dashboard/project"><AiFillProject />Projects</Link>
-                    </li>
-                </ul>
+                        <li className=' hover:bg-slate-600 transition-all rounded-lg'>
+                            <Link className='bg-transparent text-white' to="/dashboard/repoting"><MdOutlineTaskAlt /> Repoting</Link>
+                        </li>
+                        <li className=' hover:bg-slate-600 transition-all rounded-lg'>
+                            <Link className='bg-transparent text-white' to="/dashboard/add-reviews"><RiUserStarLine /> Add Reviews</Link>
+                        </li>
+                        <li className=' hover:bg-slate-600 transition-all rounded-lg'>
+                            <Link className='bg-transparent text-white' to="/dashboard/calendar"><AiFillProject />Calendar</Link>
+                        </li>
+                        <li className=' hover:bg-slate-600 transition-all rounded-lg'>
+                            <Link className='bg-transparent text-white' to="/dashboard/meeting"><MdVideoCall />Meeting</Link>
+                        </li>
+                        <li className=' hover:bg-slate-600 transition-all rounded-lg'>
+                            <Link className='bg-transparent text-white' to="/dashboard/project"><AiFillProject />Projects</Link>
+                        </li>
+                    </ul>
+                    <ul className='mx-auto'>
+                        {
+                            projects?.map((project: Project) => <ul>
+                                <li className='hover:bg-slate-600 transition-all py-1 text-secondary'>{project.projectName}</li>
+                            </ul>)
+                        }
+                    </ul>
+
+                </div>
 
             </div>
-        </div>
+
+
+        </>
     );
 };
 
