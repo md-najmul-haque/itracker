5f0e81e95bbf19d2c2f6bc48f6e15e9ae6de0728
John Doe <hamedhasan422421@gmail.com>
2022-08-16 07:09:57 +0300

    update on view profile section

--- src/Pages/ViewProfile/ViewProfile.tsx
+++ src/Pages/ViewProfile/ViewProfile.tsx
@@ -22,44 +22,204 @@ if(user){
 
 }
 
+const handleEdit = () => {
+    setIsEdit(true)
+}
 
+const onSubmit = (data: any) => {
+    console.log(data)
+}
 
     return (
-        <form 
-        className="my-36 bg-base-200 flex-col flex items-center py-11"
-        >
-            <div className='card flex-shrink-0 shadow-2xl bg-base-100 w-3/4 lg:w-2/3 justify-center items-center'>
-            <div className='flex items-center bg-slate-200 w-full'>
-            <div className='avatar p-5 pr-0 w-1/4'>
-                <div className='w-36 mask mask-hexagon'>
-                    {/* <img src="" alt="" /> here will be user info images */}
-                </div>
+        <form
+        onSubmit={handleSubmit(onSubmit)}
+        className=" mt-5 bg-base-200 flex-col flex items-center py-11"
+      >
+        <div className="card flex-shrink-0 shadow-2xl bg-base-100 w-3/4 lg:w-2/3 justify-center items-center">
+          <div className="flex items-center bg-slate-200 w-full">
+            <div className="avatar p-5 pr-0 w-1/4">
+              <div className=" w-36 mask mask-hexagon">
+                {/* <img src={userInfo?.photoURL} alt={userInfo?.displayName} /> */}
+              </div>
             </div>
-            
+            {/* Name Section  */}
             <div>
-                {/* <h1 className='text-4xl font-bold'></h1> here will be user info displayName */}
-
-                {/* <p></p> here will be user info email */}
+              {/* <h1 className="text-4xl font-bold">{userInfo?.displayName}</h1>
+              <p>{userInfo?.email}</p> */}
             </div>
-            </div>
-            
+          </div>
 
-            {/* Additional Info */}
-            <div className='overflow-x-auto w-full'>
+          {/* Aditional Info  */}
+          <div className="overflow-x-auto w-full">
+            {/* <!-- row 1 --> */}
+            <div className="w-full flex flex-row">
+              <div className=" px-5 lg:px-10 w-1/6 lg:w-2/12 py-5">
+                <div className="font-bold">Address</div>
+                <div className="text-sm opacity-50">City</div>
+              </div>
+              <div className=" px-10 py-5 w-5/6 lg:w-10/12">
+                {isEdit ? (
+                  <input
+                    className="input input-bordered input-secondary w-full"
+                    // name="address"
+                    // defaultValue={userInfo?.address}
+                    {...register("address", {
+                      maxLength: 500,
+                    })}
+                  />
+                  
+                ) : (
+                //   userInfo?.address
+                <div></div>
+                )}
+                <br />
 
-                <div className='w-full flex flex-row'>
-                    <div className='px-5 lg:px-10 w-1/6 lg:w-2/12 py-5'>
-                        <div className='font-bold'>Address</div>
-                        <div className='text-sm opacity-50'>City</div>
-                    </div>
-                    <div className='px-10 py-5 w-5/6 lg:w-10/12'>
-                        
-                    </div>
-                </div>
-
+                {isEdit ? (
+                  <input
+                    className="input input-bordered w-full mt-1"
+                    // defaultValue={userInfo?.city}
+                    {...register("city", { maxLength: 500 })}
+                  />
+                ) : (
+                  <span className="badge badge-ghost badge-sm">
+                    {/* {userInfo?.city} */}
+                  </span>
+                )}
+              </div>
             </div>
+            {/* <!-- row 2 --> */}
+            <div className="w-full flex flex-row">
+              <div className=" px-5 lg:px-10 w-1/6 lg:w-2/12 py-5">
+                <div className="font-bold">Phone</div>
+              </div>
+              <div className=" px-10 py-5 w-5/6 lg:w-10/12">
+                {isEdit ? (
+                  <input
+                    type="tel"
+                    className="input input-bordered input-secondary w-full"
+                    // defaultValue={userInfo?.phone}
+                    {...register("phone", { maxLength: 15 })}
+                  />
+                ) : (
+                //   userInfo?.phone
+                <div></div>
+                )}
+              </div>
             </div>
-        </form>
+            {/* <!-- row 2 --> */}
+            <div className="w-full flex flex-row">
+              <div className=" px-5 lg:px-10 w-1/6 lg:w-2/12 py-5">
+                <div className="font-bold">Facebook</div>
+              </div>
+              <div className=" px-10 py-5 w-5/6 lg:w-10/12">
+                {isEdit ? (
+                  <input
+                    type="text"
+                    className="input input-bordered input-secondary w-full"
+                    // defaultValue={userInfo?.facebook}
+                    {...register("facebook", { maxLength: 250 })}
+                  />
+                ) : (
+                  <a
+                    // href={userInfo?.facebook}
+                    target="_blank"
+                    className=" text-primary hover:underline"
+                  >
+                    {/* {userInfo?.facebook} */}
+                  </a>
+                )}
+              </div>
+            </div>
+            {/* <!-- row 2 --> */}
+            <div className="w-full flex flex-row">
+              <div className=" px-5 lg:px-10 w-1/6 lg:w-2/12 py-5">
+                <div className="font-bold">GitHub</div>
+              </div>
+              <div className=" px-10 py-5 w-5/6 lg:w-10/12">
+                {isEdit ? (
+                  <input
+                    type="text"
+                    className="input input-bordered input-secondary w-full"
+                    // defaultValue={userInfo?.github}
+                    {...register("github", { maxLength: 250 })}
+                  />
+                ) : (
+                  <a
+                    // href={userInfo?.github}
+                    className=" text-primary hover:underline"
+                    target="_blank"
+                  >
+                    {/* {userInfo?.github} */}
+                  </a>
+                )}
+              </div>
+            </div>
+            {/* <!-- row 2 --> */}
+            <div className="w-full flex flex-row">
+              <div className=" px-5 lg:px-10 w-1/6 lg:w-2/12 py-5">
+                <div className="font-bold">Linkedin</div>
+              </div>
+              <div className=" px-10 py-5 w-5/6 lg:w-10/12">
+                {isEdit ? (
+                  <input
+                    type="text"
+                    className="input input-bordered input-secondary w-full"
+                    // defaultValue={userInfo?.linkedin}
+                    {...register("linkedin", { maxLength: 250 })}
+                  />
+                ) : (
+                  <a
+                    // href={userInfo?.linkedin}
+                    target="_blank"
+                    className=" text-primary hover:underline"
+                  >
+                    {/* {userInfo?.linkedin} */}
+                  </a>
+                )}
+              </div>
+            </div>
+            {/* <!-- row 2 --> */}
+            <div className="w-full flex flex-row">
+              <div className=" px-5 lg:px-10 w-1/6 lg:w-2/12 py-5">
+                <div className="font-bold">Twitter</div>
+              </div>
+              <div className=" px-10 py-5 w-5/6 lg:w-10/12">
+                {isEdit ? (
+                  <input
+                    type="text"
+                    className="input input-bordered input-secondary w-full"
+                    // defaultValue={userInfo?.twitter}
+                    {...register("twitter", { maxLength: 250 })}
+                  />
+                ) : (
+                  <a
+                    // href={userInfo?.twitter}
+                    target="_blank"
+                    className=" text-primary hover:underline"
+                  >
+                    {/* {userInfo?.twitter} */}
+                  </a>
+                )}
+              </div>
+            </div>
+            {/* <!-- row 2 --> */}
+            <div className=" flex flex-row justify-evenly items-center px-5 lg:px-10 py-5">
+              <button
+                className="btn btn-primary"
+                disabled={isEdit}
+                onClick={handleEdit}
+              >
+                Edit
+              </button>
+              <input
+                type="submit"
+                className="btn btn-secondary"
+                disabled={!isEdit}
+              />
+            </div>
+          </div>
+        </div>
+      </form>
     );
 };
 
