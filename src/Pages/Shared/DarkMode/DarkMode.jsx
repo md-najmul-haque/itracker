0dcdb958d91a27d996c459eefd74c403fe275d4a
John Doe <hamedhasan422421@gmail.com>
2022-08-22 00:08:40 +0300

    implement darke & light icon

--- src/Pages/Shared/DarkMode/DarkMode.jsx
+++ src/Pages/Shared/DarkMode/DarkMode.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./DarkMode.css";
+import { BsMoonStars } from 'react-icons/bs';
+import { BsSun } from 'react-icons/bs';
 
 const DarkMode = () => {
+
+
     let clickedClass = "clicked"
   const body = document.body
   const lightTheme = "light"
   const darkTheme = "dark"
-  let theme:any
+  let theme;
 
   if (localStorage) {
     theme = localStorage.getItem("theme")
@@ -18,7 +22,7 @@ const DarkMode = () => {
     body.classList.add(lightTheme)
   }
 
-  const switchTheme = (e:any) => {
+  const switchTheme = (e) => {
     if (theme === darkTheme) {
       body.classList.replace(darkTheme, lightTheme)
       e.target.classList.remove(clickedClass)
@@ -32,11 +36,31 @@ const DarkMode = () => {
     }
   }
     return (
-        <button
-      className={theme === "dark" ? clickedClass : ""}
-      id="darkMode"
-      onClick={e => switchTheme(e)}
-    ></button>
+      <>
+      
+        {/* <button
+    //  className={` toggle ${theme === "dark" ? clickedClass : ""}`}
+ 
+      >click</button> */}
+
+<label class="swap swap-rotate" >
+  
+  {/* <!-- this hidden checkbox controls the state --> */}
+  <input type="checkbox"     id="darkMode"
+     onClick={e => switchTheme(e)}/>
+  
+  {/* <!-- sun icon --> */}
+    {/* <!-- sun icon --> */}
+    <BsSun class="swap-on fill-current w-5 h-5 text-orange-300 "/>  
+
+  
+    {/* <!-- moon icon --> */}
+    <BsMoonStars class="swap-off fill-current w-5 h-5 text-green-300" />
+
+  
+</label>
+    
+      </>
     );
 };
 
