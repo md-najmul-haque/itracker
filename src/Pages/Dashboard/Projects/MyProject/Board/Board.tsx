befee7857fbf2273025fc2893b7d19160e3cffde
Md Najmul Haque <najmulhaque008@gmail.com>
2022-08-22 17:34:06 +0600

    task list added to the board

--- src/Pages/Dashboard/Projects/MyProject/Board/Board.tsx
+++ src/Pages/Dashboard/Projects/MyProject/Board/Board.tsx
@@ -1,78 +1,14 @@
-import { useState } from 'react';
-import { DragDropContext, Draggable, Droppable, DropResult } from 'react-beautiful-dnd';
-import AddTask from './AddTask';
-import AddTaskInput from './AddTaskInput';
-import TaskCard from './TaskCard';
-import Title from './Title';
-// import { List } from './list.type'
-
-type List = {
-
-    title: string,
-    id: string,
-    cards: {
-        id: string,
-        task: string
-    }[]
-
-}
+import TaskList from './TaskList';
 
 
 const Board = () => {
 
-    const [lists, setLists] = useState([])
-
-    // const onDragEnd = (result: DropResult) => {
-    //     const { source, destination } = result
-    //     if (!destination) return
-    // }
-
-    fetch('/lists.json')
-        .then(res => res.json())
-        .then(data => setLists(data))
-
     return (
-        <div className='flex'>
-            {/* <DragDropContext onDragEnd={onDragEnd}>
-                <Droppable droppableId="app" type='list' direction='horizontal'>
-                    {(provided) => (
-                        <div className="wrapper" ref={provided.innerRef}>
-                            {lists.map((list: List) => {
-                                return (
-                                    <TaskList key={list.id} list={list} />
-                                )
-                            })}
-                        </div>
-                    )}
-                </Droppable>
-            </DragDropContext> */}
-
-            {
-                lists.map((list: List) => {
-
-                    return <div>
-                        <div className="w-80 ml-5 border-4 border-secondary bg-secondary p-2">
-                            < Title key={list.id} title={list.title} />
-                            <TaskCard key={list.id} cards={list.cards} />
-                            <AddTask />
-                        </div>
-                    </div>
-
-                })
-            }
-
-
-
-
-
-
-
-
-
-
+        <div >
+            <TaskList />
         </div>
-    );
-};
 
+    )
+}
 
 export default Board;
---
+++ src/Pages/Dashboard/Projects/MyProject/Board/TaskList.tsx
@@ -0,0 +1,61 @@
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+import { BsThreeDotsVertical } from "react-icons/bs";
+
+type List = {
+    id: string,
+    title: string,
+    cards: {
+        task: string
+    }[]
+
+}
+
+
+const TaskList = () => {
+    const [open, setOpen] = useState(false)
+
+    const { data: lists, isLoading, error, refetch } = useQuery(['lists'], () =>
+        fetch('http://localhost:5000/getList')
+            .then(res => res.json())
+    )
+    refetch();
+
+
+    return (<div className='flex flex-row'>
+        {
+            lists?.map((list: List) => {
+                const cards = list.cards
+                return <div className="w-80 ml-5 border-4 border-secondary bg-secondary p-2">
+                    <div>
+                        {
+                            open ?
+                                (<div className=''>
+                                    <input className='bg-secondary-focus border-none py-1 w-full' onBlur={() => setOpen(!open)} value={'To Do'} />
+                                </div>) :
+                                (<div className='flex justify-between items-center px-1'>
+                                    <h2 className='pr-5 font-bold text-xl' onClick={() => { setOpen(!open) }}>{list.title}</h2>
+                                    <BsThreeDotsVertical />
+                                </div>)
+                        }
+                    </div>
+                    <div>
+                        {
+                            cards.map((card, index) => {
+                                return <p key={index} className="bg-white  shadow-md p-2 my-2">{card?.task}</p>
+                            })
+                        }
+
+                    </div >
+
+                </div>
+
+
+            })
+        }
+
+    </div>
+    );
+};
+
+export default TaskList;
