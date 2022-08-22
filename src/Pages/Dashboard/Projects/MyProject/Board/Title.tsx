961e1be61c5fc19eba6a650157e1bcefe45e9938
Md Najmul Haque <najmulhaque008@gmail.com>
2022-08-22 13:57:21 +0600

    fake data added and fetch for board

--- src/Pages/Dashboard/Projects/MyProject/Board/Title.tsx
+++ src/Pages/Dashboard/Projects/MyProject/Board/Title.tsx
@@ -1,11 +1,13 @@
 import TaskCard from './TaskCard';
-import { TaskListPros } from './list.type'
 import { Draggable, Droppable } from 'react-beautiful-dnd';
 import { useState } from 'react';
 import { BsThreeDotsVertical } from 'react-icons/bs';
 
+type TitleProps = {
+    title: string,
+}
 
-const Title = () => {
+const Title = ({ title }: TitleProps) => {
 
     const [open, setOpen] = useState(false)
 
@@ -18,7 +20,7 @@ const Title = () => {
                         <input className='bg-secondary-focus border-none py-1 w-full' onBlur={() => setOpen(!open)} value={'To Do'} />
                     </div>) :
                     (<div className='flex justify-between items-center px-1'>
-                        <h2 className='pr-5 font-bold text-xl' onClick={() => { setOpen(!open) }}>To Do</h2>
+                        <h2 className='pr-5 font-bold text-xl' onClick={() => { setOpen(!open) }}>{title}</h2>
                         <BsThreeDotsVertical />
                     </div>)
             }
