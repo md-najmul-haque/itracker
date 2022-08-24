7d935a64ae08b21d1ecfb538129826867c334df1
Md Najmul Haque <najmulhaque008@gmail.com>
2022-08-24 10:17:00 +0600

    alert added in the project list section

--- src/Pages/Dashboard/Projects/MyProject/List/AddIssue.tsx
+++ src/Pages/Dashboard/Projects/MyProject/List/AddIssue.tsx
@@ -41,6 +41,12 @@ const AddIssue = ({ setModal }: AddIssueProps) => {
         })
             .then(res => res.json())
             .then(project => console.log(project))
+        swal({
+            title: "Congrats!",
+            text: "Task added Successfully!",
+            icon: "success",
+        });
+        setModal(false);
 
     };
 
