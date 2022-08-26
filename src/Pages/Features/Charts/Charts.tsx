61016a9e9d8e06c564a6da8088be2b5d09d19e96
Md Najmul Haque <najmulhaque008@gmail.com>
2022-08-26 16:43:00 +0600

    feature route updated

--- src/Pages/Features/Charts/Charts.tsx
+++ src/Pages/Features/Charts/Charts.tsx
@@ -4,11 +4,11 @@ import chart from '../../../Assets/features/chart1.jpg';
 const Charts = () => {
   return (
     <section className="max-w-full bg-white px-10 py-20 dark:bg-black dark:text-white">
-      <div className="flex justify-center items-center my-10 grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
-        <div className="flex justify-center items-center py-3">
+      <div className="flex flex-col lg:flex-row justify-center items-center container mx-auto my-10">
+        <div className="flex-1 py-3">
           <img src={chart} alt="" />
         </div>
-        <div className="lg:px-5">
+        <div className="flex-1 lg:px-5">
           <h1 className="text-4xl font-bold">CHARTS AND GRAPHS</h1>
           <p className="py-6 font-bold">
             Visualize data to troubleshoot problems instantly. Zoom in on
--- src/Pages/Features/Fixissue/Fixissue.tsx
+++ src/Pages/Features/Fixissue/Fixissue.tsx
@@ -1,19 +1,18 @@
-import React from 'react';
 import fixissue from '../../../Assets/features/issue.webp';
 
 const Fixissue = () => {
 
-    return (
-        <div className="hero bg-white dark:bg-black dark:text-white">
-  <div data-aos="zoom-in" data-aos-delay="200" data-aos-duration="1000" className="hero-content text-center">
-    <div className='lg:w-3/5'>
-      <h1 className="text-3xl font-bold py-3">ISSUE TRACKING FIX ISSUE FASTER</h1>
-      <p className="py-6 font-semibold">Make the process of how your team records and tracks bugs easier. iTracker turns reported issues into clear, easy-to-follow outlines so you can dive into details faster.</p>
-      <img src={fixissue} alt="" className="w-full pb-5" />
+  return (
+    <div className="hero bg-white dark:bg-black dark:text-white">
+      <div className="hero-content text-center">
+        <div className='lg:w-3/5'>
+          <h1 className="text-3xl font-bold py-3">ISSUE TRACKING FIX ISSUE FASTER</h1>
+          <p className="py-6 font-semibold">Make the process of how your team records and tracks bugs easier. iTracker turns reported issues into clear, easy-to-follow outlines so you can dive into details faster.</p>
+          <img src={fixissue} alt="" className="w-full pb-5" />
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-    );
+  );
 };
 
 export default Fixissue;
